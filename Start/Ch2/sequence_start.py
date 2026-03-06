19e8319c1cb07ff7d188e7204b78f9af4a8998c1
pjamili <p.jamili@gmail.com>
2026-03-06 13:07:50 -0500

    initial commit

--- Start/Ch2/sequence_start.py
+++ Start/Ch2/sequence_start.py
@@ -3,21 +3,34 @@
 
 # Sequences: Lists and Tuples
 # These are -- surprise -- sequences of values
-
-
+mylist = [1, 2, "two", False, 3.14]
+# print(len(mylist))
 # to access a member of a sequence type, use []
-
-
+# print(mylist[1])
+# print(mylist[-1])
 # add a list to another list
+# mylist[0] = 100
+# print(mylist)
+# mylist.append(200)
+# print(mylist)
 
+# another_list = [4, 5, 6]
+# mylist = mylist + another_list
+# print(mylist)
 
+# mystr = "This is a string"
+# print(mystr[5:7])
 # use slices to get parts of a sequence
-
+# print(mylist[1:4:2])
+# print(mylist[::2])
 
 # you can use slices to reverse a sequence
+# print(mylist[::-1])
 
 
 # Tuples are like lists, but they are immutable
+mytuple = (1, 2, "two", False, 3.14)
+print(mytuple[1])
 
 
 # Sets are also sequences, but they contain unique values
--- Start/Ch2/variables_start.py
+++ Start/Ch2/variables_start.py
@@ -10,12 +10,24 @@ mystr = "This is a string"
 mybool = True
 
 # We can display the content of a variable using the print() function
-
+print(myint)
+print(myfloat)
+print(mystr)
+print(mybool)
 
 # Operators are used to perform operations on variables
+print('='*100)
 
-
+print(myint + myfloat)
+print(myint / myfloat)
+print(myint % 3)
+print(mystr, ' and this is more string', 'xxxxx')
 # Logical and comparison operators 
+print('='*100)
+print(myint == 10)
+print(myfloat != 10)
+print(myint< 20 and myfloat > 20)
+print(myint < 20 or myfloat < 10)
 
 
 # re-declaring a variable works
